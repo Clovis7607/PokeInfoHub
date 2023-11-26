df6079b962a9823a499731b45e379fe1332a4cd1
Clovis <fjig5116@gmail.com>
2023-11-26 12:06:50 +0800

    Initial Commit

---
+++ PokeInfoHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeInfoHub
+//
+//  Created by 吳謹安 on 2023/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
